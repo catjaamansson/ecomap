aff66bb7079028db262217978ec029601b44cc7e
Oskar Olofsson <oskarjolofsson@gmail.com>
2025-12-06 19:42:28 +0100

    Routes to pages instead of just one page in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route} from 'react-router-dom'
 import './App.css'
 
+//import pages
+import Frontpage from './pages/frontpage.jsx'
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div
-      style={{
-        backgroundImage: "url('/leaf.svg')",
-        backgroundRepeat: 'repeat',
-        backgroundSize: '200px',
-        width: '100vw',
-        height: '100vh',
-        margin: 0,
-        padding: 0,
-        backgroundColor: '#356d4fff',
-      }}
-    >
-      <h1 className="font-bold" style={{ fontFamily: 'serif', letterSpacing: '0.05em', color: '#223D2D', fontSize: '120px', margin: 0, padding: 0 }}>EcoMap</h1>
-      <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', padding: '20px', alignItems: 'center' }}>
-        <button style={{ width: '150px', padding: '15px', textAlign: 'center', backgroundColor: '#597D68' }}>Hydrologi</button>
-        <button style={{ width: '150px', padding: '15px', textAlign: 'center', backgroundColor: '#597D68' }}>Ekologi</button>
-        <button style={{ width: '150px', padding: '15px', textAlign: 'center', backgroundColor: '#597D68' }}>Fältstudie</button>
-        <button style={{ width: '150px', padding: '15px', textAlign: 'center', backgroundColor: '#597D68' }}>4</button>
-      </div>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Frontpage />} />
+        <Route path ='/map' element={<div>Map page</div>} />
+      </Routes>
+    </>
   )
 }
 

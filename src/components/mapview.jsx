3183638d6f32930b06effed71938920cf89ec4da
Oskar Olofsson <oskarjolofsson@gmail.com>
2025-12-12 20:51:41 +0100

    Working leaflet map

--- src/components/mapview.jsx
+++ src/components/mapview.jsx
@@ -1,18 +1,27 @@
-import { MapContainer, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+// Fix for marker icons
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
+})
 
 function Mapview() {
   return (
-    <div style={{ height: '100vh', width: '100vw' }}>
-      <MapContainer
-        center={[59.3293, 18.0686]}  // Stockholm
-        zoom={10}
-        style={{ height: '100%', width: '100%' }}
-      >
+    <div style={{ height: '500px', width: '100%' }}>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: '100%', width: '100%' }}>
         <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="&copy; OpenStreetMap contributors"
         />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
       </MapContainer>
     </div>
   );
